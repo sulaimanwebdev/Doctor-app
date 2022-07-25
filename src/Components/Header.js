d7f226d1ac300ea2e79c7aae47453b3a43765884
Muhammad Sulaiman <trendingofyt@gmail.com>
2022-07-26 02:47:58 +0500

    asd

---
+++ src/Components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+    <div className="w-full bg-white">
+        <div className="w-full px-5 bg-white max-w-[1350px] mx-auto">
+            
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Header
