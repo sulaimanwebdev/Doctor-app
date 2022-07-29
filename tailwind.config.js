67f0c21515b0b12bb58c287c4c0ef71c93e107f0
Muhammad Sulaiman <trendingofyt@gmail.com>
2022-07-30 02:53:38 +0500

    Allhamdulliah. All set and done

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,16 @@ module.exports = {
     extend: {},
     screens: {
       'sm': '640px',
+      's2': '540px',
+
       // => @media (min-width: 640px) { ... }
 
       'md': '768px',
       // => @media (min-width: 768px) { ... }
 
       'lg': '1024px',
+      'lg2': '1060px',
+
       // => @media (min-width: 1024px) { ... }
 
       'xl': '1280px',
